e21cd758835339d82a6a02297ea7b9df4e129736
Nick Rodriguez <nrodrig1@gmail.com>
2025-08-11 17:20:11 +0000

    test run.py setup

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# run.py
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from app_package import create_app
 
 app = create_app()
@@ -6,6 +10,8 @@ if __name__ == '__main__':
     # app.run()
     # app.run(host="0.0.0.0", port=8003)
     port = int(os.environ.get("FLASK_RUN_PORT"))
-    # host = os.environ.get("FLASK_RUN_HOST")
-    # app.run(host=host, port=port)
-    app.run(port=port)
+    host = os.environ.get("FLASK_RUN_HOST")
+    print(f"port: {port}")
+    print(f"host: {host}")
+    app.run(host=host, port=port)
+    # # app.run(port=port)
